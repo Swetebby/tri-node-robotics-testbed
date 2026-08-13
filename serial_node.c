0802904fee0d9e67e2f293411be803c6e02f3afd
Tebby <georgwberg@gmail.com>
2026-08-13 14:35:40 +0200

    added firmware dir

---
+++ serial_node.c
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+
+int send_serial_data(int baud, char* buffer) {
+    if (baud < 9600) {
+        return -1;
+    }
+    printf("Sending: %s at %d\n", buffer, baud);
+    return 0;
+}
---
+++ test_node.py
@@ -0,0 +1,11 @@
+﻿import sys, os
+
+
+def calculate_robot_speed(distance, time):
+    if time == 0:
+        return None
+    speed = distance / time
+    return speed
+
+
+print(calculate_robot_speed(100, 5))
